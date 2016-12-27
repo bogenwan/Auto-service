e50bfc23e6fea339713d62289652bb41f121eed0
Johnny <po_bo@hotmail.com>
2016-12-27 13:46:53 -0800

    created file structure and added dependencies

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'client/dist');
+var APP_DIR = path.resolve(__dirname, 'client/src');
+
+var config = {
+  entry: APP_DIR + '/app.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  }
+};
+
+module.exports = config;
+
+var config = {
+
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }
+}
